cc472941759b720e8e2ef3580e50f9c3990f55aa
Mijashh <amijash@gmail.com>
2025-01-13 15:18:17 +0545

    feat: enable pages and set light color mode preference in Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  pages: true,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   eslint: { config: { standalone: false } },
+  colorMode: {
+    preference: 'light',
+  },
   modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@nuxt/eslint']
 })
